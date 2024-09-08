37cd1a926c2e94fdbb9a397415f3f98f56193719
Nathan Hüsken <nathan@huesken.org>
2024-09-08 21:39:35 +0200

    use dockers compose plugin, not docker compose standalone

--- init-database.sh
+++ init-database.sh
@@ -24,34 +24,34 @@ if [ -z "$PAPERLESS_PRIVATE_DB_PASSWORD" ]; then
 fi
 
 echo "starting database withdocker-cpomose"
-docker-compose up --no-start
-docker-compose start db
+docker compose up --no-start
+docker compose start db
 
 echo "creating dbs and users"
-docker-compose exec -T db psql <<-EOSQL
+docker compose exec -T db psql <<-EOSQL
 SELECT 'CREATE DATABASE paperless_work'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'paperless_work')\gexec
 EOSQL
-docker-compose exec -T db psql <<-EOSQL
+docker compose exec -T db psql <<-EOSQL
 SELECT 'CREATE USER paperless_work'
 WHERE NOT EXISTS (SELECT FROM pg_user WHERE usename = 'paperless_work')\gexec
 EOSQL
-docker-compose exec -T db psql <<-EOSQL
+docker compose exec -T db psql <<-EOSQL
 ALTER USER paperless_work PASSWORD '${PAPERLESS_WORK_DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON DATABASE paperless_work TO paperless_work;
 \c paperless_work;
 GRANT ALL ON SCHEMA public TO paperless_work;
 EOSQL
 
-docker-compose exec -T db psql <<-EOSQL
+docker compose exec -T db psql <<-EOSQL
 SELECT 'CREATE DATABASE paperless_private'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'paperless_private')\gexec
 EOSQL
-docker-compose exec -T db psql <<-EOSQL
+docker compose exec -T db psql <<-EOSQL
 SELECT 'CREATE USER paperless_private'
 WHERE NOT EXISTS (SELECT FROM pg_user WHERE usename = 'paperless_private')\gexec
 EOSQL
-docker-compose exec -T db psql <<-EOSQL
+docker compose exec -T db psql <<-EOSQL
 ALTER USER paperless_private PASSWORD '${PAPERLESS_PRIVATE_DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON DATABASE paperless_private TO paperless_private;
 \c paperless_private;
@@ -60,4 +60,4 @@ EOSQL
 
 echo "Stopping the system"
 
-docker-compose down
+docker compose down
