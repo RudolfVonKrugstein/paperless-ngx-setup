82b35c13eb58a72f0245e2ee536a418346b2d364
Nathan Hüsken <nathan@huesken.org>
2024-09-10 10:04:31 +0200

    fix timestamp verify command

--- scripts/post-consumption.sh
+++ scripts/post-consumption.sh
@@ -5,12 +5,26 @@ set -e
 echo "Begin post consumption script"
 
 echo "Creating signing request"
-openssl ts -query -data "$DOCUMENT_SOURCE_PATH" -no_nonce -sha512 -cert -out /usr/src/paperless/timestamps/${DOCUMENT_ID}.tsq
+openssl ts \
+  -query \
+  -data "$DOCUMENT_SOURCE_PATH" \
+  -no_nonce \
+  -sha512 \
+  -cert \
+  -out /usr/src/paperless/timestamps/${DOCUMENT_ID}.tsq
 
 echo "Signing"
-curl -H "Content-Type: application/timestamp-query" --data-binary "@/usr/src/paperless/timestamps/${DOCUMENT_ID}.tsq" https://freetsa.org/tsr >/usr/src/paperless/timestamps/${DOCUMENT_ID}.tsr
+curl -H "Content-Type: application/timestamp-query" \
+  --data-binary "@/usr/src/paperless/timestamps/${DOCUMENT_ID}.tsq" \
+  https://freetsa.org/tsr \
+  >/usr/src/paperless/timestamps/${DOCUMENT_ID}.tsr
 
 echo "Verify it"
-openssl ts -verify -in file.tsr -queryfile /usr/src/paperless/timestamps/${DOCUMENT_ID}.tsr -CAfile /scripts/cacert.pem -untrusted /scripts/tsa.crt
+openssl ts \
+  -verify \
+  -in /usr/src/paperless/timestamps/${DOCUMENT_ID}.tsr \
+  -queryfile /usr/src/paperless/timestamps/${DOCUMENT_ID}.tsq \
+  -CAfile /scripts/cacert.pem \
+  -untrusted /scripts/tsa.crt
 
 echo "End post consumption script"
