30f7ec51ea63a5178a543745039a3da7eae70616
Nathan Hüsken <nathan@huesken.org>
2024-09-10 15:18:06 +0200

    store timestamps hashes in files witht the sha512 as the name, becuase we can not be sure about the orignals filename

--- scripts/post-consumption.sh
+++ scripts/post-consumption.sh
@@ -4,6 +4,11 @@ set -e
 
 echo "Begin post consumption script"
 
+echo "Get the timestamp filename"
+# we use the sha512 for the timestamp file name,
+# because all other names may change.
+export TS_FNAME=$(sha512sum ${DOCUMENT_SOURCE_PATH} | awk '{print $1}')
+
 echo "Creating signing request"
 openssl ts \
   -query \
@@ -11,23 +16,23 @@ openssl ts \
   -no_nonce \
   -sha512 \
   -cert \
-  -out /usr/src/paperless/timestamps/${DOCUMENT_ID}.tsq
+  -out /usr/src/paperless/timestamps/${TS_FNAME}.tsq
 
 echo "Signing"
 curl -H "Content-Type: application/timestamp-query" \
-  --data-binary "@/usr/src/paperless/timestamps/${DOCUMENT_ID}.tsq" \
+  --data-binary "@/usr/src/paperless/timestamps/${TS_FNAME}.tsq" \
   https://freetsa.org/tsr \
-  >/usr/src/paperless/timestamps/${DOCUMENT_ID}.tsr
+  >/usr/src/paperless/timestamps/${TS_FNAME}.tsr
 
 echo "Show timestamp information"
 openssl ts -reply \
-  -in /usr/src/paperless/timestamps/${DOCUMENT_ID}.tsr \
+  -in /usr/src/paperless/timestamps/${TS_FNAME}.tsr \
   -text
 
 echo "Verify it"
 openssl ts \
   -verify \
-  -in /usr/src/paperless/timestamps/${DOCUMENT_ID}.tsr \
+  -in /usr/src/paperless/timestamps/${TS_FNAME}.tsr \
   -data "$DOCUMENT_SOURCE_PATH" \
   -CAfile /scripts/cacert.pem \
   -untrusted /scripts/tsa.crt
